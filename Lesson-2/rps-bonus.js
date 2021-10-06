4c78f8e44b221dab23f64ea965a23d5393fdb2ff
Matthew Johnson <matthew.dave.johnson@gmail.com>
2021-10-06 00:56:15 -0700

    Added bonus features to rock, paper, scissors program

---
+++ Lesson-2/rps-bonus.js
@@ -0,0 +1,55 @@
+// Add bonus features to the Rock Paper Scissors program
+
+const readline = require('readline-sync');
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const WINNING_COMBOS = {
+  rock: ['scissors', 'lizard'],
+  paper: ['rock', 'spock'],
+  scissors: ['paper', 'lizard'],
+  lizard: ['paper', 'spock'],
+  spock: ['rock', 'scissors'],
+};
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function playerWins(choice, computerChoice) {
+  return WINNING_COMBOS[choice].includes(computerChoice);
+}
+
+function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  if (playerWins(choice, computerChoice)) {
+    prompt('You win!');
+  } else if (choice === computerChoice) {
+    prompt("It's a tie!");
+  } else {
+    prompt('Computer wins!');
+  }
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(choice, computerChoice);
+
+  prompt('Do you want to play again (y/n)');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+}
