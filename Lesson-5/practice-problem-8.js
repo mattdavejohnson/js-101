43d866287305c42f539453a9512d1147958ce51e
Matthew Johnson <matthew.dave.johnson@gmail.com>
2021-11-10 18:09:00 -0800

    Solution for practice problem 8

---
+++ Lesson-5/practice-problem-8.js
@@ -0,0 +1,22 @@
+// Using forEach, write code to ouput all vowels from the strings in the
+// arrays. Don't use a for or while loop.
+
+let obj = {
+  first: ['the', 'quick'],
+  second: ['brown', 'fox'],
+  third: ['jumped'],
+  fourth: ['over', 'the', 'lazy', 'dog'],
+};
+
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+Object.values(obj).forEach((arr) => {
+  arr
+    .join('')
+    .split('')
+    .forEach((letter) => {
+      if (vowels.includes(letter)) {
+        console.log(letter);
+      }
+    });
+});
