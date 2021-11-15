0391c1ce4ebdf98489507598242e05eb99dca127
Matthew Johnson <matthew.dave.johnson@gmail.com>
2021-11-15 00:36:32 -0800

    Added player and computer choice logic and other helper functions

--- Lesson-6/tictactoe.js
+++ Lesson-6/tictactoe.js
@@ -1,5 +1,15 @@
 // Create a tic tac toe game
 
+const readline = require('readline-sync');
+
+const INITIAL_MARKER = ' ';
+const HUMAN_MARKER = 'X';
+const COMPUTER_MARKER = 'O';
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
 function displayBoard(board) {
   console.log('');
   console.log('     |     |');
@@ -16,16 +26,46 @@ function displayBoard(board) {
   console.log('');
 }
 
-let board = {
-  1: 'X', // top left
-  2: ' ', // top center
-  3: ' ', // top right
-  4: ' ', // middle left
-  5: 'O', // center
-  6: ' ', // middle right
-  7: ' ', // bottom left
-  8: ' ', // bottom center
-  9: 'X', // bottom right
-};
+function initializeBoard() {
+  let board = {};
+
+  for (let square = 1; square <= 9; square++) {
+    board[String(square)] = INITIAL_MARKER;
+  }
+
+  return board;
+}
+
+function emptySquares(board) {
+  return Object.keys(board).filter((key) => board[key] === INITIAL_MARKER);
+}
+
+function playerChoosesSquare(board) {
+  let square;
+
+  while (true) {
+    prompt(`Choose a square (${emptySquares(board).join(', ')})`);
+    square = readline.question().trim();
+
+    if (emptySquares(board).includes(square)) break;
+
+    prompt("Sorry, that's not a valid choice.");
+  }
+
+  board[square] = HUMAN_MARKER;
+}
+
+function computerChoosesSquare(board) {
+  let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+
+  let square = emptySquares(board)[randomIndex];
+  board[square] = COMPUTER_MARKER;
+}
+
+let board = initializeBoard();
+displayBoard(board);
+
+playerChoosesSquare(board);
+computerChoosesSquare(board);
 
 displayBoard(board);
