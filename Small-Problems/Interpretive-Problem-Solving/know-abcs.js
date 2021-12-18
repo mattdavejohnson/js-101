001dde18166ab63bfaabd41e61c46474f793c200
Matthew Johnson <matthew.dave.johnson@gmail.com>
2021-12-18 00:47:50 -0800

    Third Interpretive Problem Solving problem

---
+++ Small-Problems/Interpretive-Problem-Solving/know-abcs.js
@@ -0,0 +1,106 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable id-length */
+// Write a function that takes a word string as an argument and returns true
+// if the word can be spelled using the set of blocks, false otherwise.
+
+/*
+
+P:
+
+Input: A string
+Output: A boolean. If the word conforms to the rules then return true.
+
+Understand the Problem:
+
+Given the following collection of spelling blocks:
+
+B:O  X:K  D:Q  C:P  N:A  G:T  R:E  F:S  J:W  H:U  V:I  L:Y  Z:M
+
+If the following rules apply to the passed in string, return true.
+
+- Cannot use both letters from any given block.
+- Can only use each block once.
+
+
+E:
+
+isBlockWord('BATCH');      // true
+isBlockWord('BUTCH');      // false
+isBlockWord('jest');       // true
+
+
+D:
+
+Arrays
+Objects
+
+
+A:
+
+1. Declare variable 'letterBlocks' and initialize to object containing all
+letter blocks.
+2. Declare variable 'testArray' and initialize to empty array.
+3. Loop through string argument. In each iteration:
+  - Declare variable 'currentLetter' and initialize to the current letter
+    lowercase.
+  - Declare variable 'currentLetterBlock' and initialize to the current
+    letter block looked up in 'letterBlocks' object.
+  - Check if the 'currentLetterBlock' exists in 'testArray'.
+    - If no, push 'currentLetterBlock' to 'testArray'.
+    - If yes, return false.
+4. Return true.
+
+
+C:
+
+ */
+
+function isBlockWord(word) {
+  let letterBlocks = {
+    a: 'na',
+    b: 'bo',
+    c: 'cp',
+    d: 'dq',
+    e: 're',
+    f: 'fs',
+    g: 'gt',
+    h: 'hu',
+    i: 'vi',
+    j: 'jw',
+    k: 'xk',
+    l: 'ly',
+    m: 'zm',
+    n: 'na',
+    o: 'bo',
+    p: 'cp',
+    q: 'dq',
+    r: 're',
+    s: 'fs',
+    t: 'gt',
+    u: 'hu',
+    v: 'vi',
+    w: 'jw',
+    x: 'xk',
+    y: 'ly',
+    z: 'zm',
+  };
+
+  let testArray = [];
+
+  for (let index = 0; index < word.length; index += 1) {
+    let currentLetter = word[index].toLowerCase();
+    let currentLetterBlock = letterBlocks[currentLetter];
+
+    if (testArray.includes(currentLetterBlock)) {
+      return false;
+    } else {
+      testArray.push(currentLetterBlock);
+    }
+  }
+
+  return true;
+}
+
+console.log(isBlockWord('BATCH'));
+console.log(isBlockWord('BUTCH'));
+console.log(isBlockWord('jest'));
