239d2eca6a6f938c860369410e1b05d9a144198f
Matthew Johnson <matthew.dave.johnson@gmail.com>
2021-12-20 17:47:39 -0800

    Second batch of Debugging JS problems

---
+++ Small-Problems/Debugging-JS/member-directory.js
@@ -0,0 +1,72 @@
+/*
+Fix the Member Directory code so that the input validation works as expected.
+
+Original Code:
+
+let memberDirectory = {
+  'Jane Doe': '323-8293',
+  'Margaret Asbury': '989-1111',
+  'Callum Beech': '533-9090',
+  'Juanita Eastman': '424-1919',
+};
+
+function isValidName(name) {
+  return (/^\w+ \w+$/).test(name);
+}
+
+function isValidPhone(phone) {
+  return (/^\d{3}-\d{4}$/).test(phone);
+}
+
+function validMemberInfo(name, phone) {
+  return isValidName(name) && isValidPhone(phone);
+}
+
+function addMember(name, phone) {
+  if (validMemberInfo(name, phone)) {
+    memberDirectory[name] = phone;
+  } else {
+    console.log('Invalid member information.');
+  }
+}
+
+addMember('Laura Carlisle', '444-2223');
+addMember('Rachel Garcia', '232-1191');
+addMember('Earl 5mith', '331-9191');
+
+console.log(memberDirectory);
+*/
+
+let memberDirectory = {
+  'Jane Doe': '323-8293',
+  'Margaret Asbury': '989-1111',
+  'Callum Beech': '533-9090',
+  'Juanita Eastman': '424-1919',
+};
+
+function isValidName(name) {
+  // FIXED: The regex was not correct. It allowed for numbers and underscore.
+  return /^[a-z]+ [a-z]+$/i.test(name);
+}
+
+function isValidPhone(phone) {
+  return /^\d{3}-\d{4}$/.test(phone);
+}
+
+function validMemberInfo(name, phone) {
+  return isValidName(name) && isValidPhone(phone);
+}
+
+function addMember(name, phone) {
+  if (validMemberInfo(name, phone)) {
+    memberDirectory[name] = phone;
+  } else {
+    console.log('Invalid member information.');
+  }
+}
+
+addMember('Laura Carlisle', '444-2223');
+addMember('Rachel Garcia', '232-1191');
+addMember('Earl 5mith', '331-9191');
+
+console.log(memberDirectory);
